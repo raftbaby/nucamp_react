e8e72671dd49bd955f6d0e591e27d3245dae9a86
Nicole <raftbaby@gmail.com>
2020-06-06 06:55:10 -0700

    Presentational and Container Components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -44,11 +44,14 @@ class CampsiteInfo extends Component {
 
     render(){
         if (this.props.campsite) {
-            return(
-                <div className= "row">
-                {this.renderCampsite(this.props.campsite)}
-                {this.renderComments(this.props.campsite.comments)}
-                </div>);
+            return (
+                <div className="container">
+                    <div className="row">
+                        {this.renderCampsite(this.props.campsite)}
+                        {this.renderComments(this.props.campsite.comments)}
+                    </div>
+                </div>
+            );
         }
         return <div />;
     }
