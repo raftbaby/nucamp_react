ea9edf38660712281c809a17f13f068e3accb53b
Nicole <raftbaby@gmail.com>
2020-06-20 08:45:14 -0700

    Fetch from Server

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,7 +9,7 @@ import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponents';
 import About from './AboutComponent';
-import { addComment, fetchCampsites } from '../redux/ActionCreators';
+import { addComment, fetchCampsites, fetchComments, fetchPromotions } from '../redux/ActionCreators';
 import { actions } from 'react-redux-form';
 
 
@@ -25,13 +25,18 @@ const mapStateToProps = state => {
 const mapDispatchToProps = {
     addComment: (campsiteId, rating, author, text) => (addComment(campsiteId, rating, author, text)),
     fetchCampsites: () => (fetchCampsites()),
-    resetFeedbackForm: () => (actions.reset('feedbackForm'))
-}
+    resetFeedbackForm: () => (actions.reset('feedbackForm')),
+    fetchComments: () => (fetchComments()),
+    fetchPromotions: () => (fetchPromotions())
+};
+
 
 class Main extends Component {
 
     componentDidMount() {
         this.props.fetchCampsites();
+        this.props.fetchComments();
+        this.props.fetchPromotions();
     }
 
     render() {
@@ -39,12 +44,14 @@ class Main extends Component {
         const HomePage = () => {
             return (
                 <Home
-                    campsite={this.props.campsites.campsites.filter(campsite => campsite.featured)[0]}
-                    campsitesLoading={this.props.campsites.isLoading}
-                    campsitesErrMess={this.props.campsites.errMess}
-                    promotion={this.props.promotions.filter(promotion => promotion.featured)[0]}
-                    partner={this.props.partners.filter(partner => partner.featured)[0]}
-                />
+                campsite={this.props.campsites.campsites.filter(campsite => campsite.featured)[0]}
+                campsitesLoading={this.props.campsites.isLoading}
+                campsitesErrMess={this.props.campsites.errMess}
+                partner={this.props.partners.filter(partner => partner.featured)[0]}
+                promotion={this.props.promotions.promotions.filter(promotion => promotion.featured)[0]}
+                promotionLoading={this.props.promotions.isLoading}
+                promotionErrMess={this.props.promotions.errMess}
+            />
             );
         }
 
@@ -54,9 +61,10 @@ class Main extends Component {
                     campsite={this.props.campsites.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
                     isLoading={this.props.campsites.isLoading}
                     errMess={this.props.campsites.errMess}
-                    comments={this.props.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+                    comments={this.props.comments.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+                    commentsErrMess={this.props.comments.errMess}
                     addComment={this.props.addComment}
-                />
+                />      
             );
         };
         
